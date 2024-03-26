9c6be2fec96538fd9e7caca5e698c28c9ef8fead
sahaniaditya <adityasahani893@gmail.com>
2024-03-26 15:59:41 +0530

    project initialized

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React,{ useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-4xl text-red-400'>apple</h1>
+        
+    </>
+  )
+}
+
+export default App
